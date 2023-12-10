5e2f51f6404635c1197577816d54e4b527725f3c
Evandro <41019684+Evandro-Saints@users.noreply.github.com>
2023-12-10 11:19:27 -0300

    Update test.php

--- test.php
+++ test.php
@@ -10,38 +10,3 @@ fwrite($f,$c);
 fclose($f);
 ?>
 [alert(1)]
-
-
-
-
-<!---<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<style>
-  @keyframes colorChange {
-    0% { color: red; }
-    25% { color: blue; }
-    50% { color: green; }
-    75% { color: orange; }
-    100% { color: purple; }
-  }
-
-  .animated-text {
-    font-size: 24px;
-    font-weight: bold;
-    animation: colorChange 5s infinite;
-  }
-  
-  background-color {
-    { color: black; }
-  }
-</style>
-<title>Animated Text</title>
-</head>
-<body>
-  <div class="animated-text">Evandro-Saints!</div>
-</body>
-</html>
-
